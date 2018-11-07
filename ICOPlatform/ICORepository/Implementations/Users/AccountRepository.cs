3f2cfdd80a54a577201c5ff93caaaf468793d8ef
Hai Nguyen <nnhaigl@gmail.com>
2018-11-08 00:34:44 +0700

    add account repository

---
+++ ICOPlatform/ICORepository/Implementations/Users/AccountRepository.cs
@@ -0,0 +1,29 @@
+﻿using ICOCore.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICORepository.Implementations.Users
+{
+    public class AccountRepository : Repository<Account>
+    {
+        public bool IsUsernameExisted(string username)
+        {
+            return GetTable().Count(x => x.Username == username) > 0;
+        }
+
+        public bool IsUsernameExisted(int accId, string username)
+        {
+            if (!IsUsernameExisted(username)) return false;
+
+            if (accId <= 0) return true;
+            Account acc = Get(accId);
+
+            if (acc == null) return false;
+            return !(acc.Username == username);
+        }
+
+    }
+}
