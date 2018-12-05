02a65a98eb6c8ab7c3b3cbffc4edb57d8423e61d
Phuoc Nguyen <phuocns@imt-soft.com>
2018-12-06 01:17:28 +0700

    Create api flow

---
+++ ICOPlatform/ICOCore/Infrastructures/Enums/UserReferralStatusEnum.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ICOCore.Infrastructures.Enums
+{
+    public enum UserReferralStatusEnum : byte
+    {
+        /// <summary>
+        /// Chưa click vào link confirm trong mail
+        /// </summary>
+        NOT_CONFIRMED = 0,
+        /// <summary>
+        /// Sau Khi click vào link trong mail cần chờ ng giới thiệu phê duyệt
+        /// </summary>
+        PENDING = 1,
+        /// <summary>
+        /// Người giới thiệu đã phê duyệt.
+        /// </summary>
+        ACTIVE = 2,
+        /// <summary>
+        /// Admin đã xác nhận thông tin cá nhân (PID , Ảnh v.v.)
+        /// </summary>
+        COMPLETE = 3,
+        INACTIVE = 4
+    }
+}
