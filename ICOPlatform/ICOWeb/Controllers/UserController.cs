6c5a563c6c25e7abbd7cdfc01d31aef3706a4161
Hai Nguyen <nnhaigl@gmail.com>
2018-12-06 02:37:31 +0700

    add css login page

--- ICOPlatform/ICOWeb/Controllers/UserController.cs
+++ ICOPlatform/ICOWeb/Controllers/UserController.cs
@@ -42,106 +42,106 @@ namespace ICOWeb.Controllers
                 }
             }
 
-            //try
-            //{
-            //    if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
-            //    {
-            //        result = false;
-            //    }
+            try
+            {
+                if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+                {
+                    result = false;
+                }
 
-            //    ICOCore.Repositories.UserInfo user = _accountService.Login(username, password);
-            //    if (null == user || user.ReferralStatus == (int)UserReferralStatusEnum.INACTIVE)
-            //    {
-            //        ApplicationContext.IncreaseLoginFailCount();
-            //        result = false;
-            //    }
-            //    else
-            //    {
-            //        // ------------------ kiểm tra captcha
-            //        bool isInProduction = CommonConstants.IS_IN_PRODUCTION;
+                ICOCore.Repositories.UserInfo user = _accountService.Login(username, password);
+                if (null == user || user.ReferralStatus == (int)UserReferralStatusEnum.INACTIVE)
+                {
+                    ApplicationContext.IncreaseLoginFailCount();
+                    result = false;
+                }
+                else
+                {
+                    // ------------------ kiểm tra captcha
+                    bool isInProduction = CommonConstants.IS_IN_PRODUCTION;
 
-            //        if (isInProduction)
-            //        {
-            //            var response = g_recaptcha_response;  //Request["g-recaptcha-response"];
-            //            if (string.IsNullOrWhiteSpace(response))
-            //            {
-            //                captchaErrorMess = "The response parameter is missing...";
-            //                isCaptchaErr = true;
-            //            }
-            //            else
-            //            {
-            //                //secret that was generated in key value pair
-            //                const string secret = "6Ld1YgkUAAAAAIbSQSBeM2uSV7sN5dUc3AWXV4-a";
+                    if (isInProduction)
+                    {
+                        var response = g_recaptcha_response;  //Request["g-recaptcha-response"];
+                        if (string.IsNullOrWhiteSpace(response))
+                        {
+                            captchaErrorMess = "The response parameter is missing...";
+                            isCaptchaErr = true;
+                        }
+                        else
+                        {
+                            //secret that was generated in key value pair
+                            const string secret = "6Ld1YgkUAAAAAIbSQSBeM2uSV7sN5dUc3AWXV4-a";
 
-            //                var client = new WebClient();
-            //                var reply =
-            //                    client.DownloadString(
-            //                        string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}",
-            //                    secret, response));
+                            var client = new WebClient();
+                            var reply =
+                                client.DownloadString(
+                                    string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}",
+                                secret, response));
 
-            //                var captchaResponse = JsonConvert.DeserializeObject<GoogleCaptchaResponse>(reply);
+                            var captchaResponse = JsonConvert.DeserializeObject<GoogleCaptchaResponse>(reply);
 
-            //                //when response is false check for the error message
-            //                isCapChaSuccess = captchaResponse.Success;
-            //                if (!isCapChaSuccess)
-            //                {
-            //                    isCaptchaErr = captchaResponse.ErrorCodes.Count <= 0 ? false : true;
+                            //when response is false check for the error message
+                            isCapChaSuccess = captchaResponse.Success;
+                            if (!isCapChaSuccess)
+                            {
+                                isCaptchaErr = captchaResponse.ErrorCodes.Count <= 0 ? false : true;
 
-            //                    if (isCaptchaErr)
-            //                    {
-            //                        // nếu có lỗi capcha
-            //                        var error = captchaResponse.ErrorCodes[0].ToLower();
-            //                        switch (error)
-            //                        {
-            //                            case ("missing-input-secret"):
-            //                                captchaErrorMess = "The secret parameter is missing.";
-            //                                break;
-            //                            case ("invalid-input-secret"):
-            //                                captchaErrorMess = "The secret parameter is invalid or malformed.";
-            //                                break;
-            //                            case ("missing-input-response"):
-            //                                captchaErrorMess = "The response parameter is missing.";
-            //                                break;
-            //                            case ("invalid-input-response"):
-            //                                captchaErrorMess = "The response parameter is invalid or malformed.";
-            //                                break;
-            //                            default:
-            //                                captchaErrorMess = "Error occured. Please try again";
-            //                                break;
-            //                        }
-            //                    }
-            //                }
-            //            }
-            //        }
+                                if (isCaptchaErr)
+                                {
+                                    // nếu có lỗi capcha
+                                    var error = captchaResponse.ErrorCodes[0].ToLower();
+                                    switch (error)
+                                    {
+                                        case ("missing-input-secret"):
+                                            captchaErrorMess = "The secret parameter is missing.";
+                                            break;
+                                        case ("invalid-input-secret"):
+                                            captchaErrorMess = "The secret parameter is invalid or malformed.";
+                                            break;
+                                        case ("missing-input-response"):
+                                            captchaErrorMess = "The response parameter is missing.";
+                                            break;
+                                        case ("invalid-input-response"):
+                                            captchaErrorMess = "The response parameter is invalid or malformed.";
+                                            break;
+                                        default:
+                                            captchaErrorMess = "Error occured. Please try again";
+                                            break;
+                                    }
+                                }
+                            }
+                        }
+                    }
 
-            //        if ((!isInProduction) || (isInProduction && !isCaptchaErr))
-            //        {
-            //            ViewBag.Success = true;
-            //            // save cookie
-            //            if (remember_me == "on")
-            //            {
-            //                FormsAuthentication.SetAuthCookie(username, true);
-            //            }
-            //            else
-            //            {
-            //                FormsAuthentication.SetAuthCookie(username, false);
-            //            }
+                    if ((!isInProduction) || (isInProduction && !isCaptchaErr))
+                    {
+                        ViewBag.Success = true;
+                        // save cookie
+                        if (remember_me == "on")
+                        {
+                            FormsAuthentication.SetAuthCookie(username, true);
+                        }
+                        else
+                        {
+                            FormsAuthentication.SetAuthCookie(username, false);
+                        }
 
-            //            result = true;
+                        result = true;
 
-            //            //Lưu Application Context
-            //            var userLevelService = new UserLevelService();
-            //            user.LevelName = userLevelService.GetByCode(user.LevelCode).Name;
-            //            ApplicationContext.CurrentUser = user;
-            //        }
+                        //Lưu Application Context
+                        var userLevelService = new UserLevelService();
+                        user.LevelName = userLevelService.GetByCode(user.LevelCode).Name;
+                        ApplicationContext.CurrentUser = user;
+                    }
 
-            //        // ------------------ /kiểm tra captcha
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    _logger.Error(ex);
-            //}
+                    // ------------------ /kiểm tra captcha
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex);
+            }
 
             return Json(new { IsSuccess = result, CaptchaErrorMess = captchaErrorMess });
         }
