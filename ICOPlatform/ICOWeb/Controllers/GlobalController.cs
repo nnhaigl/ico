665006a314168d4748f04631d6354d6a856fc5e4
Hai Nguyen <nnhaigl@gmail.com>
2018-12-09 09:00:11 +0700

    error handle

---
+++ ICOPlatform/ICOWeb/Controllers/GlobalController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ICOWeb.Controllers
+{
+    public class GlobalController : Controller
+    {
+        public ActionResult InternalServerError()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        public ActionResult OpenSoon()
+        {
+            return View();
+        }
+    }
+}
